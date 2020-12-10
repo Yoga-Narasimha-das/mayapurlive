a931ba210a9d9b108775001463d70c37dcd185ac
Timofey Pchelintsev <tpchelintsev@gmail.com>
2020-12-10 21:32:13 +0530

    Dancing with ENV variables

--- api/begin.js
+++ api/begin.js
@@ -2,8 +2,7 @@ import { createNowBeginHandler } from 'netlify-cms-oauth-provider-node'
 
 // export default createNowBeginHandler({}, { useEnv: true })
 export default createNowBeginHandler({
-  debug: process.env.GATSBY_DEBUG,
-  completeUrl: process.env.GATSBY_COMPLETE_URL,
+  completeUrl: `${process.env.GATSBY_ORIGIN}/api/complete`,
   oauthClientID: process.env.GATSBY_GITHUB_OAUTH_CLIENT_ID,
   oauthClientSecret: process.env.GATSBY_GITHUB_OAUTH_CLIENT_SECRET,
   oauthProvider: process.env.GATSBY_OAUTH_PROVIDER,
--- api/complete.js
+++ api/complete.js
@@ -2,8 +2,7 @@ import { createNowCompleteHandler } from 'netlify-cms-oauth-provider-node'
 
 // export default createNowCompleteHandler({}, { useEnv: true })
 export default createNowCompleteHandler({
-  debug: process.env.GATSBY_DEBUG,
-  completeUrl: process.env.GATSBY_COMPLETE_URL,
+  completeUrl: `${process.env.GATSBY_ORIGIN}/api/complete`,
   oauthClientID: process.env.GATSBY_GITHUB_OAUTH_CLIENT_ID,
   oauthClientSecret: process.env.GATSBY_GITHUB_OAUTH_CLIENT_SECRET,
   oauthProvider: process.env.GATSBY_OAUTH_PROVIDER,
