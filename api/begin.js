1be1f10d725f2f53ec9dd4cfa4264a89043f6498
Timofey Pchelintsev <tpchelintsev@gmail.com>
2020-12-10 22:11:47 +0530

    Fix redirect_url mismatch

--- api/begin.js
+++ api/begin.js
@@ -1,10 +1,11 @@
 import { createNowBeginHandler } from 'netlify-cms-oauth-provider-node'
 
+const baseUrl = process.env.GATSBY_ORIGIN.startsWith('http') ? process.env.GATSBY_ORIGIN : `https://${process.env.GATSBY_ORIGIN}`
 // export default createNowBeginHandler({}, { useEnv: true })
 export default createNowBeginHandler({
-  completeUrl: `${process.env.GATSBY_ORIGIN}/api/complete`,
+  completeUrl: `${baseUrl}/api/complete`,
   oauthClientID: process.env.GATSBY_GITHUB_OAUTH_CLIENT_ID,
   oauthClientSecret: process.env.GATSBY_GITHUB_OAUTH_CLIENT_SECRET,
   oauthProvider: process.env.GATSBY_OAUTH_PROVIDER,
-  origin: process.env.GATSBY_ORIGIN
+  origin: baseUrl
 })
